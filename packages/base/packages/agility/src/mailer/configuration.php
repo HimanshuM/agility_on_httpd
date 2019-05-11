d7486d4ae65a17cfb622f2ae79abb96590945f20
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-05-11 22:23:51 +0530

    Added assetHost and urlHost for image and link tags construction in emails

--- packages/base/packages/agility/src/mailer/configuration.php
+++ packages/base/packages/agility/src/mailer/configuration.php
@@ -9,10 +9,48 @@ use Phpm\Exceptions\TypeExceptions\InvalidTypeException;
 
 		use Accessor;
 
+		protected $assetHost;
+		protected $urlHost;
 		protected $deliveryMethod = "mail";
 
 		function __construct() {
-			$this->notFoundResponse(ACCESSOR_NOT_FOUND_CALLBACK, "defineDeliveryMethod");
+			$this->notFoundResponse(ACCESSOR_NOT_FOUND_CALLBACK, "defaultCallback");
+		}
+
+		protected function defaultCallback($name, $args = null) {
+
+			if ($name == "deliveryMethod") {
+				return $this->defineDeliveryMethod($name, $args);
+			}
+			elseif ($name == "assetHost") {
+
+				if (!is_string($args)) {
+					return false;
+				}
+
+				if (is_null($args)) {
+					return $this->assetHost;
+				}
+				else {
+					$this->assetHost = $args;
+				}
+
+			}
+			elseif ($name == "urlHost") {
+
+				if (!is_string($args)) {
+					return false;
+				}
+
+				if (is_null($args)) {
+					return $this->urlHost;
+				}
+				else {
+					$this->urlHost = $args;
+				}
+
+			}
+
 		}
 
 		protected function defineDeliveryMethod($name, $value = null) {
