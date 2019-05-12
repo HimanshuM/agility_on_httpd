4fedfccb682ad48e1b92817b1e86f489b10d6238
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-05-12 13:48:38 +0530

    Added Mailer\Helper trait and moved email helper functions

---
+++ packages/base/packages/agility/src/mailer/helper.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Agility\Mailer;
+
+	trait Helper {
+
+		protected function attachment($source, $options = []) {
+			$this->options->addAttachments($source, $options);
+		}
+
+		function attachments($name) {
+			return $this->options->getAttachmentByName($name);
+		}
+
+		protected function addBcc($data) {
+
+			if (!empty($data["bcc"])) {
+				$this->options->addBcc($data["bcc"]);
+			}
+			if (!empty($this->defaults["bcc"])) {
+				$this->options->addBcc($this->defaults["bcc"]);
+			}
+
+		}
+
+		protected function addCc($data) {
+
+			if (!empty($data["cc"])) {
+				$this->options->addCc($data["cc"]);
+			}
+			if (!empty($this->defaults["cc"])) {
+				$this->options->addCc($this->defaults["cc"]);
+			}
+
+		}
+
+		protected function addTo($data) {
+
+			if (!empty($data["to"])) {
+				$this->options->addTo($data["to"]);
+			}
+			if (!empty($this->defaults["to"])) {
+				$this->options->addTo($this->defaults["to"]);
+			}
+			else {
+				return false;
+			}
+
+			return true;
+
+		}
+
+		protected function inlineAttachment($name, $source, $options = []){
+			$this->options->addInlineAttachments($name, $source, $options);
+		}
+
+		protected function rawAttachment($name, $source, $options = []){
+
+			$options["name"] = $name;
+			$options["raw"] = $true;
+			$this->options->addAttachments($source, $options);
+
+		}
+
+		protected function setFrom($data) {
+
+			if (!empty($data)) {
+				$this->options->setFrom($data["from"]);
+			}
+			elseif (!empty($this->defaults["from"])) {
+
+				if (is_a($this->defaults["from"], Closure::class)) {
+
+					$from = $this->defaults["from"];
+					$from = $from();
+
+				}
+				else {
+					$from = $this->defaults["from"];
+				}
+
+				$this->options->setFrom($this->defaults["from"]);
+
+			}
+
+			return !empty($this->options->from);
+
+		}
+
+		protected function setReplyTo($data) {
+
+			if (!empty($data)) {
+				$this->options->setFrom($data["replyTo"]);
+			}
+			elseif (!empty($this->defaults["replyTo"])) {
+
+				if (is_a($this->defaults["replyTo"], Closure::class)) {
+
+					$replyTo = $this->defaults["replyTo"];
+					$replyTo = $replyTo();
+
+				}
+				else {
+					$replyTo = $this->defaults["replyTo"];
+				}
+
+				$this->options->setReplyTo($this->defaults["replyTo"]);
+
+			}
+
+		}
+
+	}
+
+?>
