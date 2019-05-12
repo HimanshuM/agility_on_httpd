d0d62f8a2cbab097cd768644252af25cc71ff0e1
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-05-12 13:46:37 +0530

    Moved Attachment to helpers\ and added fill($phpMailer)

---
+++ packages/base/packages/agility/src/mailer/helpers/attachment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Agility\Mailer\Helpers;
+
+	class Attachment {
+
+		public $source;
+		public $name;
+		public $isFile = true;
+		public $contentId = false;
+		public $encoding = "base64";
+		public $mime = "";
+
+		function fill($phpMailer) {
+
+			if (!empty($this->contentId)) {
+				$phpMailer->addEmbeddedImage($this->source, $this->contentId, $this->name, $this->encoding, $this->mime);
+			}
+			elseif (!$this->isFile) {
+				$phpMailer->addStringAttachment($this->source, $this->name, $this->encoding, $this->mime);
+			}
+			else {
+				$phpMailer->addAttachment($this->source, $this->name, $this->encoding, $this->mime);
+			}
+
+		}
+
+		function url() {
+			return "CID".$this->contentId;
+		}
+
+	}
+
+?>
