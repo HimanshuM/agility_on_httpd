6bf93bf7ea20fb615d28e5f24b2d50a54775fc22
Himanshu Malpande <himanshu@jigsawacademy.com>
2019-06-04 17:51:00 +0530

    Extend HttpException

--- packages/base/packages/agility/src/http/exceptions/invalid_http_location_exception.php
+++ packages/base/packages/agility/src/http/exceptions/invalid_http_location_exception.php
@@ -2,9 +2,7 @@
 
 namespace Agility\Http\Exceptions;
 
-use Exception;
-
-	class InvalidHttpLocationException extends Exception {
+	class InvalidHttpLocationException extends HttpException {
 
 		function __construct($location, $model = false) {
 			parent::__construct("HTTP location can only be of type string".($model ? " or an Agility data model" : "").", ".gettype($location)." given");
--- packages/base/packages/agility/src/http/exceptions/invalid_http_status_exception.php
+++ packages/base/packages/agility/src/http/exceptions/invalid_http_status_exception.php
@@ -2,9 +2,7 @@
 
 namespace Agility\Http\Exceptions;
 
-use Exception;
-
-	class InvalidHttpStatusException extends Exception {
+	class InvalidHttpStatusException extends HttpException {
 
 		function __construct($status) {
 			parent::__construct("HTTP status can only be of type integer, ".gettype($status)." given");
