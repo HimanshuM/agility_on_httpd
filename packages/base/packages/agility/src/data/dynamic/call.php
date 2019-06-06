6e26e9fa75c4a953fd3b613cf4bb2a1692f4aeae
Himanshu Malpande <himanshu@jigsawacademy.com>
2019-06-06 18:32:51 +0530

    Added Dynamic\Call for allowing multiple fallback callables

---
+++ packages/base/packages/agility/src/data/dynamic/call.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Agility\Data\Dynamic;
+
+	class Call {
+
+		protected $handled = false;
+		public $args = [];
+
+		function __construct($args = []) {
+			$this->args = $args;
+		}
+
+		function handled() {
+			return $this->handled = true;
+		}
+
+	}
+
+?>
