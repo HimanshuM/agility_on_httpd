c46265caf32fc8f6108614ad2add86f8392574bf
Himanshu Malpande <himanshu@jigsawacademy.com>
2019-06-17 19:28:56 +0530

    Added feature to group JSON response on model name

--- packages/base/packages/agility/src/http/api_controller.php
+++ packages/base/packages/agility/src/http/api_controller.php
@@ -22,6 +22,8 @@ use StringHelpers\Str;
 		private $_invoked = false;
 		protected $_status = 200;
 
+		protected $jsonEncodeGrouping = "all";
+
 		function __construct() {
 
 			parent::__construct();
@@ -37,14 +39,53 @@ use StringHelpers\Str;
 			return Config::http()->concludeOnRespondedByBeforeTrigger;
 		}
 
+		protected function encodeResponse($data) {
+
+			if (is_string($data)) {
+				return $data;
+			}
+
+			if (is_a($data, Model::class)) {
+
+				if ($this->jsonEncodeGrouping != "none") {
+					$data = [$this->getModelName(get_class($data)) => $data];
+				}
+
+			}
+			elseif ($this->jsonEncodeGrouping == "all") {
+
+				if (is_a($data, Arrays::class) || is_array($data)) {
+
+					if (!empty($data[0]) && is_a($data[0], Model::class)) {
+						$data = [$this->getModelName(get_class($data[0]), true) => $data];
+					}
+
+				}
+
+			}
+
+			return json_encode($data);
+
+		}
+
+		protected function getModelName($class, $pluralize = false) {
+
+			$class = str_replace("App\\Models\\", "", $class);
+			if ($pluralize) {
+				$class = Inflect::pluralize($class);
+			}
+
+			return Str::pascalCase($class);
+
+		}
+
 		function json($data, $status = 200) {
 
 			if (is_null($data)) {
 				$data = [];
 			}
 
-			$data = is_string($data) ? $data : json_encode($data);
-			$this->respond(["json" => $data], $status);
+			$this->respond(["json" => $this->encodeResponse($data)], $status);
 
 		}
 
