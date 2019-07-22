c671eb8dc6d432a961af4355c4b80a4d2d5116e8
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-07-22 15:55:16 +0530

    Implemented db:seed command

--- packages/base/packages/agility/src/console/commands/db_command.php
+++ packages/base/packages/agility/src/console/commands/db_command.php
@@ -75,6 +75,114 @@ use StringHelpers\Str;
 
 			$this->initializeApplication($args, true);
 
+			$seed = false;
+			$pick = [];
+			if ($args->count > 0) {
+
+				foreach ($args as $arg) {
+
+					if ($arg == "all") {
+
+						$seed = true;
+						break;
+
+					}
+					else if (in_array($arg, ["development", "test", "production"])) {
+
+						$seed = true;
+						$pick = $arg;
+
+						break;
+
+					}
+					else {
+						$pick[] = $arg;
+					}
+
+				}
+
+			}
+			else {
+				$seed = true;
+			}
+
+			if ($seed) {
+				$this->executeSeedFiles();
+			}
+
+			if (is_array($pick) && count($pick)) {
+
+				foreach ($pick as $file) {
+
+					if ($file == "seeds") {
+						continue;
+					}
+
+					if ($seedsFile = $this->appPath->parent->chdir("../db/seeds")->has("$file.php")) {
+						$this->executeFile($seedsFile);
+					}
+
+					$this->executeEnvironmentFiles($file);
+
+				}
+
+			}
+			else {
+
+				if (empty($pick)) {
+
+					$seeds = $this->appPath->parent->children("../db/seeds");
+					foreach ($seeds as $seed) {
+						$this->executeFile($seed);
+					}
+
+				}
+
+				$this->executeEnvironmentFiles();
+
+			}
+
+		}
+
+		protected function executeEnvironmentFiles($file = false) {
+
+			if (!$this->appPath->parent->has("../db/seeds/".$this->_environment)) {
+				return;
+			}
+
+			if (!empty($file)) {
+
+				if ($seedsFile = $this->appPath->parent->has("../db/seeds/".$this->_environment."/$file.php")) {
+					$this->executeFile($seedsFile);
+				}
+
+			}
+			else {
+
+				$seeds = $this->appPath->parent->children("../db/seeds/$pick");
+				foreach ($seeds as $seed) {
+					$this->executeFile($seed);
+				}
+
+			}
+
+		}
+
+		protected function executeFile($file) {
+
+			echo "Running $file.php\n";
+			require_once $file;
+
+		}
+
+		protected function executeSeedFiles() {
+
+			if ($seedsFile = $this->appPath->parent->chdir("../db/seeds")->has("seeds.php")) {
+				$this->executeFile($seedsFile);
+			}
+
+			$this->executeEnvironmentFiles("seeds");
+
 		}
 
 	}
